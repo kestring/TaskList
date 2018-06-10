c30c43e1dcb5004f641175f89dbdc983d6d746bc
kestring <kestringbak@gmail.com>
2018-06-09 23:13:21 -0300

    Adição de funcionalidades
    Alteração no layout padrão das listas

--- back-end/tasklist/src/main/java/br/com/bruno/tasklist/rest/TaskListRest.java
+++ back-end/tasklist/src/main/java/br/com/bruno/tasklist/rest/TaskListRest.java
@@ -8,7 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +32,7 @@ public class TaskListRest {
         return taskRepository.save(task);
     }
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping
     public List<Task> buscarTodasTasks() {
         return taskRepository.findAll();
